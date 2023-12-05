db6f15ec525af812fea1d77b9f94361e5478b31c
Natalya M <maknatosh@gmail.com>
2023-12-05 15:15:41 +0700

    feat: burger-menu

--- coffee-house/script/Burger.js
+++ coffee-house/script/Burger.js
@@ -1,23 +1,20 @@
 const burgerIcon = document.querySelector('.burger');
 const nav = document.querySelector('.nav');
 const header = document.querySelector('.header');
+const navLink = document.querySelectorAll('.nav__link');
+
 
 const openMenu = () => {
-  burgerIcon.classList.add('burger_active-burger');
+  burgerIcon.classList.toggle('burger_active-burger');
   nav.classList.toggle('nav_active-burger');
-  header.classList.add('header_burger');
-  document.body.classList.add('body_hidden');
-  
+  header.classList.toggle('header_burger');
+  document.body.classList.toggle('body_hidden');
 };
-
-const closeMenu = () => {
-  burgerIcon.classList.remove('burger_active-burger');
-  header.classList.remove('header_burger');
-  document.body.classList.remove('body_hidden');
-};
+;
 
 export const burgerControl = () => {
-
-
   burgerIcon.addEventListener('click', openMenu);
+  navLink.forEach(link => {
+    link.addEventListener('click', openMenu)
+  })
 };
