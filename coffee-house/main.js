f0747d7afafdc2b939c503347b6cae5319d5a470
Natalya M <maknatosh@gmail.com>
2023-12-05 16:41:50 +0700

    feat: getDataGategories for Menu

--- coffee-house/main.js
+++ coffee-house/main.js
@@ -1,11 +1,13 @@
 import 'normalize.css';
 import { burgerControl } from './script/Burger';
+import { menuCategoriesControl } from './script/menuCategoriesControl';
 import './style.scss';
 
 console.log(111);
 
 const init = () => {
   burgerControl();
+  menuCategoriesControl();
 };
 
 init();
---
+++ coffee-house/script/MenuCategoriesControl.js
@@ -0,0 +1,50 @@
+const menuBtnWrap = document.querySelector('.menu__btn-wrap')
+const btnsType = document.querySelectorAll('.menu__button');
+const list = document.querySelector('.menu__list');
+
+const createItem = ({ img, title, descript, price }) => {
+  const li = document.createElement('li');
+  li.className = 'menu__item menu-card';
+  li.insertAdjacentHTML('afterbegin',
+  `
+    <div class="menu-card__img-wrap">
+      <img class="menu-card__img" src="${img}" alt="${title}">
+    </div>            
+    <div class="menu-card__content-wrap">
+      <h3 class="menu-card__title">${title}</h3>
+      <p class="menu-card__text">${descript}</p>
+      <p class="menu-card__price">${price}</p>
+    </div>     
+  `)
+  return li;
+};
+
+const getData = (type) => {
+  
+  fetch('./data/menuCategoriesData.json')                  
+    .then(response => response.json())
+    .then(data => {
+      const [{ types }] = data.filter(item => item.name === type);
+      console.log('result: ', types);
+      
+      list.append(...types.map(createItem))
+      // console.log(types.map(createItem));      
+      console.log(list);
+      // menuBtnWrap.append(list)      ;
+
+    });
+}
+
+export const menuCategoriesControl = () => {
+
+  btnsType.forEach(btn => {
+    btn.addEventListener('click', () => {
+      list.innerHTML = '';
+      getData(btn.textContent.toLowerCase());
+    });
+  })
+
+  
+
+  // console.log(data);
+};
