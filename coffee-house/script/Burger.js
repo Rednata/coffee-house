53bbee3a1db01f77e2c641b7b559b153858dbf28
Natalya M <maknatosh@gmail.com>
2023-12-05 15:00:32 +0700

    feat: smooth burger

--- coffee-house/script/Burger.js
+++ coffee-house/script/Burger.js
@@ -1,17 +1,23 @@
 const burgerIcon = document.querySelector('.burger');
 const nav = document.querySelector('.nav');
-const logo = document.querySelector('.logo');
 const header = document.querySelector('.header');
 
+const openMenu = () => {
+  burgerIcon.classList.add('burger_active-burger');
+  nav.classList.toggle('nav_active-burger');
+  header.classList.add('header_burger');
+  document.body.classList.add('body_hidden');
+  
+};
+
+const closeMenu = () => {
+  burgerIcon.classList.remove('burger_active-burger');
+  header.classList.remove('header_burger');
+  document.body.classList.remove('body_hidden');
+};
 
 export const burgerControl = () => {
-  const openMenu = () => {
-    burgerIcon.classList.add('burger_active');
-    nav.classList.add('nav_active-burger');
-    logo.classList.add('logo_active-burger');
-    header.classList.add('header_burger');
-    document.body.classList.add('body_hidden');
-  };
+
 
   burgerIcon.addEventListener('click', openMenu);
 };
