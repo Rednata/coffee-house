71e2e12efcd7e3f577351690187bd9003fa00da2
Natalya M <maknatosh@gmail.com>
2023-12-10 08:07:16 +0700

    feat: add active for bar

--- coffee-house/script/Carousel.js
+++ coffee-house/script/Carousel.js
@@ -3,18 +3,17 @@ const itemsCarousel = document.querySelectorAll('.favourite__item');
 const innerWrapCarousel = document.querySelector('.favourite__inner-wrap');
 const btnLeft = document.querySelector('.favourite__button_left');
 const btnRight = document.querySelector('.favourite__button_right');
+const bars = document.querySelectorAll('.bar__button');
+console.log('bars: ', bars);
+
 
 // const widthCarousel = 480;
 const widthCarousel = 520;
+const widthBar = 40;
 
-const moveLeft = (animaSlider) => {
-  
+const moveLeft = (animaSlider) => {  
   animaSlider.forEach(anima => {    
     anima.cancel() ;
-    // anima.addEventListener('onfinish', () => {
-    //   console.log('FINISH!');
-    // })
-
   });
   let pos = listCarousel.dataset.count || 0;
   if (pos <= 0) {
@@ -23,18 +22,16 @@ const moveLeft = (animaSlider) => {
   pos--;      
   listCarousel.dataset.count = pos;
   listCarousel.style.transform = `translateX(-${widthCarousel * pos}px)`;  
+  // bars[pos].style.width = '40px';
+  bars.forEach(bar => bar.classList.remove('bar__button_active'))
+  bars[pos].classList.add('bar__button_active');
+  
 };
 
 const moveRight = (animaSlider) => {  
-
   animaSlider.forEach(anima => {    
     anima.cancel() ;
-    // anima.addEventListener('onfinish', () => {
-    //   console.log('FINISH!');
-    // })
-
   });
-
   let pos = listCarousel.dataset.count || 0;  
   pos++;      
   if (pos >=3) {
@@ -42,23 +39,8 @@ const moveRight = (animaSlider) => {
   }  
   listCarousel.dataset.count = pos;
   listCarousel.style.transform = `translateX(${-widthCarousel * pos}px)`;
-};
-
-
-
-export const carouselControl1 = () => {
-  btnLeft.addEventListener('click', moveLeft);
-  btnRight.addEventListener('click', moveRight);
-
-  const animaSlider = animaControl(listCarousel);
-
-  listCarousel.addEventListener('mouseover', () => {
-    animaSlider.pause();      
-  })
-
-  listCarousel.addEventListener('mouseout', () => {
-    animaSlider.play()
-  })
+  bars.forEach(bar => bar.classList.remove('bar__button_active'))
+  bars[pos].classList.add('bar__button_active');
 };
 
 const carouselInfinite = () => {
@@ -132,26 +114,99 @@ const carouselInfinite = () => {
   return [animaSlider0, animaSlider1, animaSlider2];
 }
 
-export const carouselControl = () => {
+const barAnima1 = () => {
+  const timings0 = {
+    duration: 3000,
+    fill: 'forwards',
+    iterations: 1,  
+    easing: 'cubic-bezier(0.1, 0.7, 1, 0.1)',
+  }
+    const timings1 = {
+    duration: 3000,
+    fill: 'forwards',
+    iterations: 1,  
+    easing: 'cubic-bezier(0.1, 0.7, 1, 0.1)',
+  }
+    const timings2 = {
+      duration: 3000,
+      fill: 'forwards',
+      iterations: 1,  
+      easing: 'cubic-bezier(0.1, 0.7, 1, 0.1)',
+  }
+  const keyframes0 = [
+    { transform: "translateX(300px)"},      
+    { transform: "translateX(-480px)"},
+  ];
+  
+  const keyframes1 = [
+    { transform: "translateX(-250px)"},      
+    { transform: "translateX(-1000px)"},
+  ];
+  
+  const keyframes2 = [
+    { transform: "translateX(-560px)"},      
+    { transform: "translateX(-1550px)"},
+  ];
+  
+  const effect0 = new KeyframeEffect(itemsCarousel[0], keyframes0, timings0);
+  const animaSlider0 = new Animation(effect0, itemsCarousel[0].ownerDocument.timeline);
+  animaSlider0.play();
+  
+  const effect1 = new KeyframeEffect(itemsCarousel[1], keyframes1, timings1);
+  const animaSlider1 = new Animation(effect1, itemsCarousel[1].ownerDocument.timeline);
+  
+  const effect2 = new KeyframeEffect(itemsCarousel[2], keyframes2, timings2);
+  const animaSlider2 = new Animation(effect2, itemsCarousel[2].ownerDocument.timeline);
+  
+  animaSlider0.addEventListener('finish', () => {
+    animaSlider1.play();
+  })
+  
+  animaSlider1.addEventListener('finish', () => {
+    animaSlider2.play();
+  })
+  
+  animaSlider2.addEventListener('finish', () => {
+    animaSlider0.play();
+  })
+  
+  listCarousel.addEventListener('mouseover', () => {    
+    if (animaSlider0.playState === 'running') {
+      animaSlider0.pause()
+    }
+    if (animaSlider1.playState === 'running') {
+      animaSlider1.pause()
+    }
+    if (animaSlider2.playState === 'running') {
+      animaSlider2.pause()
+    }
+  })
+  
+  return [animaSlider0, animaSlider1, animaSlider2];
+};
 
+export const carouselControl = () => {
   const animaSlider = carouselInfinite();
   const [animaSlider0, animaSlider1, animaSlider2] = animaSlider;
 
-btnLeft.addEventListener('click', () => {
-  moveLeft(animaSlider);
-});
-btnRight.addEventListener('click', () => {
-  moveRight(animaSlider)});
+  // const animaBar = animaBar();
+  // const [animaSlider0, animaSlider1, animaSlider2] = animaSlider;
 
-listCarousel.addEventListener('mouseout', () => {
-  if (animaSlider0.playState === 'paused') {
-    animaSlider0.play()
-  }
-  if (animaSlider1.playState === 'paused') {
-    animaSlider1.play()
-  }
-  if (animaSlider2.playState === 'paused') {
-    animaSlider2.play()
-  }
-})
+  btnLeft.addEventListener('click', () => {
+    moveLeft(animaSlider);
+  });
+  btnRight.addEventListener('click', () => {
+    moveRight(animaSlider)});
+
+  listCarousel.addEventListener('mouseout', () => {
+    if (animaSlider0.playState === 'paused') {
+      animaSlider0.play()
+    }
+    if (animaSlider1.playState === 'paused') {
+      animaSlider1.play()
+    }
+    if (animaSlider2.playState === 'paused') {
+      animaSlider2.play()
+    }
+  })
 };
