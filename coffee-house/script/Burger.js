6ec45ca704af8f3c10206ab3801e62ffad4c0752
Rednata <maknatosh@gmail.com>
2024-06-27 19:27:27 +0700

    fix: burger Control

--- coffee-house/script/Burger.js
+++ coffee-house/script/Burger.js
@@ -3,18 +3,27 @@ const nav = document.querySelector('.nav');
 const header = document.querySelector('.header');
 const navLink = document.querySelectorAll('.nav__link');
 
+const closeMenu = () => {
+  burgerIcon.classList.remove('burger_active-burger');
+  nav.classList.remove('nav_active-burger');
+  header.classList.remove('header_burger');
+  document.body.classList.remove('body_hidden');
+};
 
 const openMenu = () => {
-  burgerIcon.classList.toggle('burger_active-burger');
-  nav.classList.toggle('nav_active-burger');
-  header.classList.toggle('header_burger');
-  document.body.classList.toggle('body_hidden');
+  if (!(burgerIcon.classList.contains('burger_active-burger'))) {
+    burgerIcon.classList.add('burger_active-burger');
+    nav.classList.add('nav_active-burger');
+    header.classList.add('header_burger');
+    document.body.classList.add('body_hidden');
+    navLink.forEach(link => {
+      link.addEventListener('click', closeMenu)
+    });
+  } else {
+    closeMenu()
+  }
 };
-;
 
 export const burgerControl = () => {
   burgerIcon.addEventListener('click', openMenu);
-  navLink.forEach(link => {
-    link.addEventListener('click', openMenu)
-  })
 };
